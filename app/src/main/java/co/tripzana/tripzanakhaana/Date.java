da08ed9834863e106d6ff9e5bb7a295356b99125
Srikant14 <srikant.sg14@gmail.com>
2017-07-09 18:21:19 +0530

    date picker has been added

--- app/src/main/java/co/tripzana/tripzanakhaana/Date.java
+++ app/src/main/java/co/tripzana/tripzanakhaana/Date.java
@@ -2,9 +2,18 @@ package co.tripzana.tripzanakhaana;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
+
 public class Date extends AppCompatActivity {
+
+    DatePicker picker;
+    Button displayDate;
+    TextView textview1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +23,34 @@ public class Date extends AppCompatActivity {
 
         TextView tt = (TextView) findViewById(R.id.textView2);
         tt.setText(getIntent().getExtras().getString("username"));
+
+        textview1=(TextView)findViewById(R.id.textView1);
+        picker=(DatePicker)findViewById(R.id.datePicker1);
+        displayDate=(Button)findViewById(R.id.button1);
+
+        textview1.setText(getCurrentDate());
+
+        displayDate.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                textview1.setText(getCurrentDate());
+            }
+
+        });
     }
+    public String getCurrentDate(){
+        StringBuilder builder=new StringBuilder();
+        builder.append("Current Date: ");
+        builder.append((picker.getMonth() + 1)+"/");//month is 0 based
+        builder.append(picker.getDayOfMonth()+"/");
+        builder.append(picker.getYear());
+        return builder.toString();
+    }
+   // @Override
+   /* public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;*/
+
 }
+
